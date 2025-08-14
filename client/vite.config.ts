1972767911f9e1cdd03ccdd999aeb912422f090e
shabman <avia.shabbyman@gmail.com>
2025-08-14 23:40:28 +0100

    shadcn init

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            "argon2-browser": path.resolve(
-                __dirname,
-                "node_modules/argon2-browser/dist/argon2-bundled.min.js",
-            ),
-        },
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
     },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "argon2-browser": path.resolve(
+        __dirname,
+        "node_modules/argon2-browser/dist/argon2-bundled.min.js"
+      ),
+    },
+  },
 });
